b4d5d5f2c9d264756851a10c93522e43ed1477b8
Hasan Raza <140344786+razajp@users.noreply.github.com>
2026-09-05 09:13:56 -0700

    refactor: standardize business controller error handling

--- src/controllers/business.controller.js
+++ src/controllers/business.controller.js
@@ -1,19 +1,17 @@
 import { BusinessService } from "../services/business.service.js";
 
+const send = (res, action) => {
+  try {
+    return res.json(action());
+  } catch (error) {
+    return res.status(error.status || 500).json({ message: error.message || "Business operation failed" });
+  }
+};
+
 export const BusinessController = {
-  referenceData(req, res) {
-    res.json(BusinessService.referenceData(req.user));
-  },
-  updateReferenceData(req, res) {
-    res.json(BusinessService.updateReferenceData(req.user.business_id, req.body?.reference_data));
-  },
-  ruleData(req, res) {
-    res.json(BusinessService.ruleData(req.user));
-  },
-  updateRuleData(req, res) {
-    res.json(BusinessService.updateRuleData(req.user, req.body?.rule_data));
-  },
-  invoiceCounter(req, res) {
-    res.json(BusinessService.invoiceCounter(req.user, req.query.year));
-  },
+  referenceData: (req, res) => send(res, () => BusinessService.referenceData(req.user)),
+  updateReferenceData: (req, res) => send(res, () => BusinessService.updateReferenceData(req.user, req.body?.reference_data)),
+  ruleData: (req, res) => send(res, () => BusinessService.ruleData(req.user)),
+  updateRuleData: (req, res) => send(res, () => BusinessService.updateRuleData(req.user, req.body?.rule_data)),
+  invoiceCounter: (req, res) => send(res, () => BusinessService.invoiceCounter(req.user, req.query.year)),
 };
