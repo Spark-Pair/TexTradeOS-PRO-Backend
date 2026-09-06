460f06771c741de287b32ab33c33ee6cdf71463f
Hasan Raza <140344786+razajp@users.noreply.github.com>
2026-09-06 02:23:11 -0700

    refactor: remove obsolete database compatibility shim

--- src/db.js
+++
@@ -1,3 +0,0 @@
-export { db, databasePath, isFreshDatabase } from "./db/connection.js";
-export { parseJson, toUserDto, toInvoiceDto } from "./db/mappers.js";
-export { defaultReferenceData, defaultAccessRules, defaultRuleData } from "./config/business-defaults.js";
